4d14705429e1d6578134e76f54fa871af18cab71
2015 <a2015@2015s-MacBook-Pro.local>
2021-12-22 10:24:06 +0700

    create port config

---
+++ env.js
@@ -0,0 +1,5 @@
+const port_host = 3000;
+const port_host_media = 8888;
+const port_rtmp = 1935;
+
+module.exports = { port_host, port_host_media, port_rtmp };
--- server/config/config.js
+++ server/config/config.js
@@ -1,18 +1,20 @@
+const config_port = require('../../env')
+
 const config = {
     server: {
         secret: 'kjVkuti2xAyF3JGCzSZTk0YWM5JhI9mgQW4rytXc',
-        port : 3333
+        port : config_port.port_host
     },
     rtmp_server: {
         rtmp: {
-            port: 1935,
+            port: config_port.port_rtmp,
             chunk_size: 60000,
             gop_cache: true,
             ping: 60,
             ping_timeout: 30
         },
         http: {
-            port: 8888,
+            port: config_port.port_host_media,
             mediaroot: './server/media',
             allow_origin: '*'
         },
